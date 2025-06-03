921d2682f86b8a03083b601acd139b05b719ee35
unknown <mariacosta0251@digitalcollege.internal>
2025-06-03 11:46:05 -0300

    first commit

---
+++ models/UsersModel.js
@@ -0,0 +1,26 @@
+class UserModel {
+    static list = [
+     { 
+        id:1,
+        login:"admin2",
+        senha: "admin321"
+
+    },
+    { 
+        id:2,
+        login:"admin3",
+        senha: "admin322"
+
+    }
+    ];
+
+    static read() {
+        return UserModel.list;
+    }
+    static getById(id){
+        return data;
+
+
+    }
+
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+ const express = require ('express');
+
+ const app = express();
+ const HOST = '127.0.0.1';
+ const PORT = 3000;
+
+ app.get('/', (req , res) => {
+    res.send('Hello world!');
+
+})
+app.listen(PORT, HOST, () => {
+    console.log(`Servidor escutando em http://${HOST}: ${PORT}`)
+})
